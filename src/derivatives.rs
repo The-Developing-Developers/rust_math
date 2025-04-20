670ba6b879fa6b20740c37b5ba8d3ca63857474e
gamma_sigma <lateo1234@gmail.com>
2025-04-20 21:06:37 +0200

    Add central and backward differentiation methods
    - Added central and backward differentiation methods to the `Derivative` module.
    - Restructured tests so that all three methods are tested for each mathematical function under test.

--- src/derivatives.rs
+++ src/derivatives.rs
@@ -37,9 +37,7 @@ impl Derivative {
 
     // TODO: GS add a copy constructor (`from`) as in `Integrator`
 
-    // TODO: GS add central and backward difference methods
-
-    /// Performs numerical differentiation using finite differences.
+    /// Performs numerical differentiation using forward difference.
     ///
     /// # Arguments
     ///
@@ -61,9 +59,50 @@ impl Derivative {
     /// ```
     pub fn forward_difference(&mut self) -> f64
     {
-        self.result = ((self.function)(self.x_coordinate + self.increment)
-            - (self.function)(self.x_coordinate))
-            / self.increment; // Forward difference formula
+        self.result = ( (self.function)(self.x_coordinate + self.increment) - (self.function)(self.x_coordinate) ) / self.increment;
+        self.result
+    }
+
+    /// Performs numerical differentiation using backward difference.
+    ///
+    /// # Arguments
+    ///
+    /// * `function` - A closure that represents the function to differentiate.
+    /// * `point` - The point at which to evaluate the derivative.
+    /// * `h` - The step size for the finite difference approximation. A smaller value will yield a more accurate result, but may lead to numerical instability. Recommended value: `1e-5` or smaller.
+    ///
+    /// # Returns
+    ///
+    /// The approximate value of the derivative at the given point.
+    /// /// # Example
+    /// /// ```
+    /// use rust_math_lib::derivatives::Derivative;
+    /// let mut derivative = Derivative::new(|x| x * x, 2.0, 1e-6);
+    /// let result = derivative.backward_difference();
+    /// /// ```
+    pub fn backward_difference(&mut self) -> f64
+    {
+        self.result = ( (self.function)(self.x_coordinate) - (self.function)(self.x_coordinate - self.increment) ) / self.increment;
+        self.result
+    }
+
+    /// Performs numerical differentiation using central difference.
+    /// # Arguments
+    /// * `function` - A closure that represents the function to differentiate.
+    /// * `point` - The point at which to evaluate the derivative.
+    /// * `h` - The step size for the finite difference approximation. A smaller value will yield a more accurate result, but may lead to numerical instability. Recommended value: `1e-5` or smaller.
+    /// # Returns
+    /// The approximate value of the derivative at the given point.
+    /// # Example
+    /// ```
+    /// use rust_math_lib::derivatives::Derivative;
+    /// let mut derivative = Derivative::new(|x| x * x, 2.0, 1e-6);
+    /// let result = derivative.central_difference();
+    /// ```
+    pub fn central_difference(&mut self) -> f64
+    {
+        let half_increment = self.increment / 2.0;
+        self.result = ( (self.function)(self.x_coordinate + half_increment) - (self.function)(self.x_coordinate - half_increment) ) / self.increment;
         self.result
     }
 }
@@ -76,25 +115,45 @@ mod tests {
 
     use crate::utils::colours::*;
 
-    /// Helper function to test differentiation with common logic.
+    // TODO: GS use a parameterised test framework (e.g., `rstest` crate) to test multiple methods and configurations in a more structured way instead of repeating the same code for each test?
+
+    /// Helper function to test differentiation using common logic.
+    ///
+    /// For each `function` passed as an argument, it will test the derivative using three methods: forward, backward, and central difference.
+    ///
+    /// /// # Arguments
+    /// * `function` - The function to differentiate.
+    /// * `x_coordinate` - The point at which to evaluate the derivative.
+    /// * `increment` - The step size for the finite difference approximation.
+    /// * `expected` - The expected value of the derivative.
+    /// * `error_tolerance` - The acceptable error tolerance for the test.
     fn test_differentiation(
         function: Function,
         x_coordinate: f64,
         increment: f64,
         expected: f64,
-        tolerance: f64,
+        error_tolerance: f64,
     ) {
         let mut derivative = Derivative::new(function, x_coordinate, increment);
-        let result = derivative.forward_difference();
-        let delta = (result - expected).abs();
-        println!(
-            "{}Result{}:    {}\n{}Expected{}:  {}\n{}Tolerance{}: {}\n{}Delta{}:     {}",
-            MAGENTA, RESET, result,
-            CYAN,    RESET, expected,
-            YELLOW,  RESET, tolerance,
-            GREEN,   RESET, delta
-        );
-        assert!(delta < tolerance);
+        let results_vec = vec![
+            ("Forward" , derivative.forward_difference()),
+            ("Backward", derivative.backward_difference()),
+            ("Cental"  , derivative.central_difference()),
+        ];
+
+        for (method_name, result) in results_vec {
+            let delta = (result - expected).abs();
+            println!(
+                "Method: {}\n  {}Result{}:    {}\n  {}Expected{}:  {}\n  {}Tolerance{}: {}\n  {}Delta{}:     {}",
+                method_name,
+                CYAN,   RESET, result,
+                YELLOW, RESET, expected,
+                GREEN,  RESET, error_tolerance,
+                WHITE,  RESET, delta
+            );
+
+            assert!(delta < error_tolerance, "Test failed for {} method", method_name);
+        }
     }
 
     #[test]
